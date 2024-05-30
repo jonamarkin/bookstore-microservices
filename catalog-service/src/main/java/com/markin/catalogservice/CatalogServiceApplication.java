62c10234e44c80b0d169525481261debfde91246
Jonathan Ato Markin <jona10711@gmail.com>
2024-05-30 17:49:30 +0200

    Pipeline fix

--- catalog-service/src/main/java/com/markin/catalogservice/CatalogServiceApplication.java
+++ catalog-service/src/main/java/com/markin/catalogservice/CatalogServiceApplication.java
@@ -7,7 +7,7 @@ import org.springframework.boot.autoconfigure.SpringBootApplication;
 public class CatalogServiceApplication {
 
     public static void main(String[] args) {
-        System.out.println("somethingg");
+        System.out.println("somethin");
         SpringApplication.run(CatalogServiceApplication.class, args);
 
     }
