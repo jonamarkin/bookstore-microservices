4f3f3580ead3adf852d9f899846d17fdc6e80c86
Jonathan Ato Markin <jona10711@gmail.com>
2024-05-30 17:44:10 +0200

    Test

--- catalog-service/src/main/java/com/markin/catalogservice/CatalogServiceApplication.java
+++ catalog-service/src/main/java/com/markin/catalogservice/CatalogServiceApplication.java
@@ -7,7 +7,9 @@ import org.springframework.boot.autoconfigure.SpringBootApplication;
 public class CatalogServiceApplication {
 
     public static void main(String[] args) {
+        System.out.println("something");
         SpringApplication.run(CatalogServiceApplication.class, args);
+
     }
 
 }
--- catalog-service/src/test/java/com/markin/catalogservice/CatalogServiceApplicationTests.java
+++ catalog-service/src/test/java/com/markin/catalogservice/CatalogServiceApplicationTests.java
@@ -2,12 +2,12 @@ package com.markin.catalogservice;
 
 import org.junit.jupiter.api.Test;
 import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.context.annotation.Import;
 
 @SpringBootTest
+@Import(ContainersConfig.class)
 class CatalogServiceApplicationTests {
 
     @Test
-    void contextLoads() {
-    }
-
+    void contextLoads() {}
 }
