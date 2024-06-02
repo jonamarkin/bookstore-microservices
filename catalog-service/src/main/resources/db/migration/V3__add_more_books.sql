4af4883bbb62d39cdff1a55113f847db26e7c78e
Jonathan Ato Markin <jona10711@gmail.com>
2024-06-02 18:20:50 +0200

    Migration added to add ore books to the database

---
+++ catalog-service/src/main/resources/db/migration/V3__add_more_books.sql
@@ -0,0 +1,11 @@
+insert into tb_products(code, name, description, image_url, price) values
+   ('PRD011', 'The Brothers Karamazov', 'A novel by Fyodor Dostoevsky, first published in 1880.', 'https://images.gr-assets.com/books/1427728126l/4934.jpg', 13.49),
+   ('PRD012', 'Crime and Punishment', 'A novel by the Russian author Fyodor Dostoevsky, first published in 1866.', 'https://images.gr-assets.com/books/1382846449l/7144.jpg', 11.99),
+   ('PRD013', 'The Picture of Dorian Gray', 'A philosophical novel by Oscar Wilde, first published in 1890.', 'https://images.gr-assets.com/books/1424596966l/5297.jpg', 10.99),
+   ('PRD014', 'Brave New World', 'A dystopian social science fiction novel by Aldous Huxley, published in 1932.', 'https://images.gr-assets.com/books/1575509280l/5129.jpg', 9.49),
+   ('PRD015', 'Jane Eyre', 'A novel by English writer Charlotte Brontë, published in 1847.', 'https://images.gr-assets.com/books/1327867269l/10210.jpg', 14.49),
+   ('PRD016', 'Wuthering Heights', 'An 1847 novel by Emily Brontë.', 'https://images.gr-assets.com/books/1388212715l/6185.jpg', 12.49),
+   ('PRD017', 'The Divine Comedy', 'An Italian narrative poem by Dante Alighieri, begun in 1308 and completed in 1320.', 'https://images.gr-assets.com/books/1427049331l/6656.jpg', 16.99),
+   ('PRD018', 'Les Misérables', 'A French historical novel by Victor Hugo, first published in 1862.', 'https://images.gr-assets.com/books/1411852091l/24280.jpg', 15.49),
+   ('PRD019', 'The Iliad', 'An ancient Greek epic poem attributed to Homer.', 'https://images.gr-assets.com/books/1388188503l/1371.jpg', 13.99),
+   ('PRD020', 'Don Quixote', 'A Spanish novel by Miguel de Cervantes, published in two parts in 1605 and 1615.', 'https://images.gr-assets.com/books/1390173285l/3836.jpg', 18.99);
