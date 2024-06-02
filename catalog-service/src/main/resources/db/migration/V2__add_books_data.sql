d4f8928cbf24f46a04695c6fb60c20b1b3f018b4
Jonathan Ato Markin <jona10711@gmail.com>
2024-06-02 18:05:54 +0200

    Books insertion migration added

---
+++ catalog-service/src/main/resources/db/migration/V2__add_books_data.sql
@@ -0,0 +1,11 @@
+INSERT INTO tb_products (code, name, description, image_url, price) VALUES
+('PRD001', 'To Kill a Mockingbird', 'A novel by Harper Lee published in 1960, which won the 1961 Pulitzer Prize.', 'https://images.gr-assets.com/books/1553383690l/2657.jpg', 10.99),
+('PRD002', '1984', 'A dystopian social science fiction novel and cautionary tale, written by the English writer George Orwell.', 'https://images.gr-assets.com/books/1532714506l/40961427.jpg', 8.99),
+('PRD003', 'Pride and Prejudice', 'A romantic novel of manners written by Jane Austen in 1813.', 'https://images.gr-assets.com/books/1320399351l/1885.jpg', 12.99),
+('PRD004', 'The Great Gatsby', 'A 1925 novel by American writer F. Scott Fitzgerald.', 'https://images.gr-assets.com/books/1490528560l/4671.jpg', 14.99),
+('PRD005', 'The Catcher in the Rye', 'A novel by J. D. Salinger, partially published in serial form in 1945–1946.', 'https://images.gr-assets.com/books/1398034300l/5107.jpg', 11.99),
+('PRD006', 'The Hobbit', 'A children fantasy novel by English author J. R. R. Tolkien.', 'https://images.gr-assets.com/books/1546071216l/5907.jpg', 9.99),
+('PRD007', 'Fahrenheit 451', 'A dystopian novel by American writer Ray Bradbury.', 'https://images.gr-assets.com/books/1383718290l/17470674.jpg', 13.99),
+('PRD008', 'Moby-Dick', 'An 1851 novel by Herman Melville.', 'https://images.gr-assets.com/books/1327940656l/153747.jpg', 15.99),
+('PRD009', 'War and Peace', 'A novel by the Russian author Leo Tolstoy, published from 1865 to 1869.', 'https://images.gr-assets.com/books/1413215930l/656.jpg', 20.99),
+('PRD010', 'The Odyssey', 'An ancient Greek epic poem attributed to Homer.', 'https://images.gr-assets.com/books/1390173285l/1381.jpg', 10.49);
