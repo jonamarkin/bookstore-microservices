6cadee9d3084a018368c4868c52ad3d1ef817104
Jonathan Ato Markin <jona10711@gmail.com>
2024-06-08 21:46:18 +0200

    Tests for get product by code and when code does not exist

--- catalog-service/src/test/java/com/markin/catalogservice/domain/ProductRepositoryTest.java
+++ catalog-service/src/test/java/com/markin/catalogservice/domain/ProductRepositoryTest.java
@@ -5,6 +5,7 @@ import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.test.autoconfigure.orm.jpa.DataJpaTest;
 import org.springframework.test.context.jdbc.Sql;
 
+import java.math.BigDecimal;
 import java.util.List;
 
 import static org.assertj.core.api.Assertions.assertThat;
@@ -26,6 +27,20 @@ class ProductRepositoryTest {
     void shouldGetAllProducts(){
         List<ProductEntity> productEntityList = productRepository.findAll();
         assertThat(productEntityList).hasSize(20);
+    }
+
+    @Test
+    void shouldGetProductByCode() {
+        ProductEntity product = productRepository.findByCode("PRD001").orElseThrow();
+        assertThat(product.getCode()).isEqualTo("PRD001");
+        assertThat(product.getName()).isEqualTo("To Kill a Mockingbird");
+        assertThat(product.getDescription()).isEqualTo("A novel by Harper Lee published in 1960, which won the 1961 Pulitzer Prize.");
+        assertThat(product.getPrice()).isEqualTo(new BigDecimal("10.99"));
+    }
+
+    @Test
+    void shouldReturnEmptyWhenProductCodeNotExists() {
+        assertThat(productRepository.findByCode("invalid_product_code")).isEmpty();
     }
 
 }
--- catalog-service/src/test/java/com/markin/catalogservice/web/controllers/ProductControllerTest.java
+++ catalog-service/src/test/java/com/markin/catalogservice/web/controllers/ProductControllerTest.java
@@ -1,11 +1,15 @@
 package com.markin.catalogservice.web.controllers;
 
 import com.markin.catalogservice.AbstractIT;
+import com.markin.catalogservice.domain.Product;
 import io.restassured.http.ContentType;
 import org.junit.jupiter.api.Test;
 import org.springframework.test.context.jdbc.Sql;
 
+import java.math.BigDecimal;
+
 import static io.restassured.RestAssured.given;
+import static org.assertj.core.api.Assertions.assertThat;
 import static org.hamcrest.Matchers.hasSize;
 import static org.hamcrest.Matchers.is;
 import static org.junit.jupiter.api.Assertions.*;
@@ -29,6 +33,38 @@ class ProductControllerTest extends AbstractIT {
                 .body("hasNext", is(true))
                 .body("hasPrevious", is(false));
 
+    }
+
+
+    @Test
+    void shouldGetProductByCode() {
+        Product product = given().contentType(ContentType.JSON)
+                .when()
+                .get("/api/products/{code}", "PRD001")
+                .then()
+                .statusCode(200)
+                .assertThat()
+                .extract()
+                .body()
+                .as(Product.class);
+
+        assertThat(product.code()).isEqualTo("PRD001");
+        assertThat(product.name()).isEqualTo("To Kill a Mockingbird");
+        assertThat(product.description()).isEqualTo("A novel by Harper Lee published in 1960, which won the 1961 Pulitzer Prize.");
+        assertThat(product.price()).isEqualTo(new BigDecimal("10.99"));
+    }
+
+    @Test
+    void shouldReturnNotFoundWhenProductCodeNotExists() {
+        String code = "invalid_product_code";
+        given().contentType(ContentType.JSON)
+                .when()
+                .get("/api/products/{code}", code)
+                .then()
+                .statusCode(404)
+                .body("status", is(404))
+                .body("title", is("Product Not Found"))
+                .body("detail", is("Product with code " + code + " not found"));
     }
 
 }
